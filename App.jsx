7e2e496cd6dda979786b11ba66dcc21ce6870163
aashish00021 <aashishbhardwaj07@icloud.com>
2025-01-07 02:31:24 +0530

    initial commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Navigation from './Src/navigation'
+
+
+export default function App() {
+  return (
+    <View>
+      <Navigation />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
